34b1df4115c07b0dd92c5e777abfd5b50c2a9e3e
Lars Solberg <lars.solberg@gmail.com>
2015-01-08 12:11:30 +0100

    Adding support for external repository, and minor cleanup

--- git-stats/generatord.sh
+++ git-stats/generatord.sh
@@ -3,6 +3,10 @@
 [[ -d /www ]] || (mkdir /www && chown nginx: /www)
 
 while true; do
+  if [[ $GIT_REPO ]]; then
+    cd /data
+    git pull
+  fi
   /usr/local/bin/git_stats generate -p /data -o /www/
   sleep ${generate_every:-3600}
 done
