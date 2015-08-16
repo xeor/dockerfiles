ad363d473ff753a3711a95f3e4a2d91883318355
Lars Solberg <lars.solberg@gmail.com>
2015-08-16 18:42:50 +0200

    Handling already running containers at startup

--- docker-acter/worker.py
+++ docker-acter/worker.py
@@ -16,24 +16,28 @@ except OSError:
     pass
 
 
-def handler():
-    for event in c.events(decode=True):
-        inspect = c.inspect_container(event['id'])
-        valid_events = [e for e in inspect['Config']['Env'] if e.startswith('ACT_')]
-        if valid_events.count == 0:
-            continue
-        for e in valid_events:
-            key, value = e.replace('ACT_', '').split('=')
-            runner_file = '{}/{}'.format(runner_dir, key)
-            if os.path.isfile(runner_file):
-                inspect_file = '{}/{}.json'.format(inspect_dir, event['id'])
-                if not os.path.isfile(inspect_file):
-                    with open(inspect_file, 'w') as fp:
-                        fp.write(json.dumps(inspect, indent=2))
-                try:
-                    subprocess.call([runner_file, value, event['id'], event['status']])
-                except OSError:
-                    # Probably not executable..
-                    continue
+def handler(docker_id, status):
+    inspect = c.inspect_container(docker_id)
+    valid_events = [e for e in inspect['Config']['Env'] if e.startswith('ACT_')]
+    if valid_events.count == 0:
+        return
+    for e in valid_events:
+        key, value = e.replace('ACT_', '').split('=')
+        runner_file = '{}/{}'.format(runner_dir, key)
+        if os.path.isfile(runner_file):
+            inspect_file = '{}/{}.json'.format(inspect_dir, docker_id)
+            if not os.path.isfile(inspect_file):
+                with open(inspect_file, 'w') as fp:
+                    fp.write(json.dumps(inspect, indent=2))
+            try:
+                subprocess.call([runner_file, value, docker_id, status])
+            except OSError:
+                # Probably not executable..
+                return
 
-handler()
+for i in c.containers(filters={'status': 'running'}):
+    handler(docker_id=i['Id'], status='running')
+
+# c.events() returns a blocking generator
+for event in c.events(decode=True):
+    handler(docker_id=event['id'], status=event['status'])
